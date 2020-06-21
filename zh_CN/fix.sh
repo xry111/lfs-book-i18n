cef1abd15feb28308a03fa7d51640778066d0720
Xℹ Ruoyao <xry111@mengyan1223.wang>
2020-06-21 17:38:38 +0800

    chapter08: merge and update the translations

--- zh_CN/fix.sh
+++ zh_CN/fix.sh
@@ -89,7 +89,10 @@ $p
 	chapter02/hostreqs.xml                       \
 	chapter06/ncurses.xml                        \
 	chapter08/glibc.xml                          \
-	chapter08/flex.xml
+	chapter08/flex.xml                           \
+	chapter08/ninja.xml                          \
+	chapter08/texinfo.xml                        \
+	chapter08/systemd.xml
 
 # Apply lfs-l10n.xml patch, if it's not applied
 grep "Simplified Chinese" stylesheets/lfs-xsl/lfs-l10n.xml ||
