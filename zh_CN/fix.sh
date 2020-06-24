d96d55419c32b7a3af351596ca151f5c7f843e02
Xℹ Ruoyao <xry111@mengyan1223.wang>
2020-06-24 15:57:36 +0800

    fix: translate "with tests" correctly

--- zh_CN/fix.sh
+++ zh_CN/fix.sh
@@ -54,7 +54,8 @@ sed -e 's/Approximate build time/估计构建时间/'               \
 
 sed -e 's/less than/不到/' \
 	-e 's/typically about/一般约/' \
-	-e 's/about \(.*\) with tests/计入测试为约 \1/' \
+	-e 's/about \(.*\) with tests/计入测试时间为约 \1/' \
+	-e 's/with tests/已计入测试时间/' \
 	-e '/encoding=/s/ISO-8859-1/UTF-8/' \
 	-i  packages.ent
 
