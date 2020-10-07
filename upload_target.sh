d1347d1be73067706d5b7bf8002c70f9256859cd
Xℹ Ruoyao <xry111@mengyan1223.wang>
2020-10-07 18:15:35 +0800

    ci: prepare to upload book according to the branch name
    Now uploading is a no-op, it will be enabled after rechecking.

---
+++ upload_target.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# only upload for branches
+case "$1" in
+	refs/heads/*)
+		;;
+	*)
+		echo "UPLOAD=no" >> $GITHUB_ENV
+		exit 0
+		;;
+esac
+
+branch="$(echo $1 | sed 's@^refs/heads/@@')"
+
+echo "${branch}"
+
+case "${branch}" in
+	*/*)
+		echo "UPLOAD=no" >> $GITHUB_ENV
+		exit 0
+		;;
+	master)
+		sysv="development"
+		sysd="systemd"
+		;;
+	*-rc*)
+		sysv="${branch}"
+		sysd="$(echo \"${branch}\" | sed 's@-@-systemd-@')"
+		;;
+	*)
+		sysv="${branch}"
+		sysd="${branch}-systemd"
+		;;
+esac
+
+echo "UPLOAD=yes" >> $GITHUB_ENV
+echo "SYSV=${sysv}" >> $GITHUB_ENV
+echo "SYSD=${sysd}" >> $GITHUB_ENV
