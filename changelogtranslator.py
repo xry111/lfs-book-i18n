934c9dbf8073a7a5ed4d001b21a706cc0160e2b2
Xi Ruoyao <xry111@mengyan1223.wang>
2022-03-03 03:19:56 +0800

    update to lfs-en @ 7c05ea2

--- changelogtranslator.py
+++ changelogtranslator.py
@@ -26,6 +26,9 @@ tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(([^ ]+) module\)\. +F
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(security fix(es)?\)\. +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2 (mise à jour de sécurité). Corrige #4',
          'zh_CN': '[#1] — 更新到 #2 (修复安全问题)。修复 #4。'})
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(security fix(es)?\)\. +Addresses (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] — Mise à jour vers #2 (mise à jour de sécurité). Corrige #4',
+         'zh_CN': '[#1] — 更新到 #2 (修复安全问题)。处理 #4。'})
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+)\. +Addresses (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2. Traiter #3',
          'zh_CN': '[#1] — 更新到 #2。处理 #3。'})
