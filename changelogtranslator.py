a6fe60fe0e06b7be6025a59437d0830b04f82d0b
Xi Ruoyao <xry111@mengyan1223.wang>
2022-04-05 20:21:40 +0800

    update to lfs-en @ b3d5df2

--- changelogtranslator.py
+++ changelogtranslator.py
@@ -23,12 +23,15 @@ tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) (\([^ ]+\))\. +Fixes (<
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(([^ ]+) module\)\. +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2 (module #3). Corrige #4',
          'zh_CN': '[#1] — 更新到 #2 (模块 #3)。修复 #4。'})
-tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(security fix(es)?\)\. +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \([sS]ecurity [fF]ix(es)?\)\. +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2 (mise à jour de sécurité). Corrige #4',
          'zh_CN': '[#1] — 更新到 #2 (修复安全问题)。修复 #4。'})
-tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \(security fix(es)?\)\. +Addresses (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \([sS]ecurity [fF]ix(es)?\)\. +Addresses (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2 (mise à jour de sécurité). Corrige #4',
          'zh_CN': '[#1] — 更新到 #2 (修复安全问题)。处理 #4。'})
+tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+) \([sS]ecurity [uU]pdate(s)?\)\. +Fixes (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
+	{'fr': '[#1] — Mise à jour vers #2 (mise à jour de sécurité). Corrige #4',
+         'zh_CN': '[#1] — 更新到 #2 (安全更新)。修复 #4。'})
 tt.append(re.compile('\[([^\]]+)\] - Updated? to ([^ ]+)\. +Addresses (<ulink [^>]+> *#[0-9]+ *</ulink>).?$', re.MULTILINE|re.DOTALL),
 	{'fr': '[#1] — Mise à jour vers #2. Traiter #3',
          'zh_CN': '[#1] — 更新到 #2。处理 #3。'})
