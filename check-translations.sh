f61d0940dadc37461b3e0d9ae097cd82ad04c4c9
Xi Ruoyao <xry111@xry111.site>
2022-09-06 23:59:38 +0800

    don't use deprecated egrep

--- check-translations.sh
+++ check-translations.sh
@@ -7,7 +7,7 @@ export LC_MESSAGES=C
 exitcode=0
 
 for i in $(find -name \*.po); do
-	if msgfmt -v $i -o /dev/null 2>&1 | tee msg | egrep -v '^[0-9]+ translated message(s?).$' > /dev/null; then
+	if msgfmt -v $i -o /dev/null 2>&1 | tee msg | grep -E -v '^[0-9]+ translated message(s?).$' > /dev/null; then
 		echo -n "$i: "
 		cat msg
 		exitcode=1
