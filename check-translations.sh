df33f32b5eefe53b389e9a97fe41b3f823f4bd95
Xi Ruoyao <xry111@xry111.site>
2023-09-02 03:16:58 +0800

    check-translations: Dump the bad file in GitHub action

--- check-translations.sh
+++ check-translations.sh
@@ -10,6 +10,9 @@ for i in $(find -name \*.po); do
 	if msgfmt -v $i -o /dev/null 2>&1 | tee msg | grep -E -v '^[0-9]+ translated message(s?).$' > /dev/null; then
 		echo -n "$i: "
 		cat msg
+		if [ "$GITHUB_ACTIONS" = "true" ]; then
+			cat $i
+		fi
 		exitcode=1
 	fi
 done
