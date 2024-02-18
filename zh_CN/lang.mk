4d852f5f59378da42daba938ab8bb20733cdb619
Xi Ruoyao <xry111@xry111.site>
2024-02-18 11:31:45 +0800

    zh_CN: Fix up pdf-fixups.sh

--- zh_CN/lang.mk
+++ zh_CN/lang.mk
@@ -65,3 +65,7 @@ zh_CN/book/$(PDF_XSL): $(LFS_EN)/$(PDF_XSL) $(FONTS_XSL) $(THIS)
 	sed -e '/<\/xsl:stylesheet>/i <xsl:include href="pdf/zh_CN-fonts.xsl"/>' \
 	    -e 's/USletter/A4/' \
 		< $< > $@
+
+zh_CN/book/pdf-fixups.sh: $(LFS_EN)/pdf-fixups.sh $(THIS)
+	mkdir -pv $(@D)
+	sed 's/monospace/Mono SC/' < $< > $@
